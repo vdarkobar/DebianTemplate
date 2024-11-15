6295e1b921c462770f1ba7984353d4232ccff1aa
Darko Vukovic <vdarkobar@gmail.com>
2024-11-15 13:02:58 +0100

    Update setup_vm.sh

--- setup_vm.sh
+++ setup_vm.sh
@@ -46,33 +46,19 @@ BRIDGE="${BRIDGE:-vmbr0}"
 VMID=$(pvesh get /cluster/nextid)
 DEFAULT_IMAGE_URL="https://cloud.debian.org/images/cloud/bookworm/latest/debian-12-nocloud-amd64.qcow2"
 IMAGE_NAME="debian-12-nocloud-amd64.qcow2"
-IMAGE_PATH="/srv/$IMAGE_NAME"
 
 # Prompt for custom image URL or use default
 read -p "Enter custom image URL or press Enter to use default [$DEFAULT_IMAGE_URL]: " IMAGE_URL
 IMAGE_URL="${IMAGE_URL:-$DEFAULT_IMAGE_URL}"
 
-# Check if Debian image file already exists in /srv
-USE_EXISTING=false
-if [[ -f "$IMAGE_PATH" ]]; then
-    read -p "Debian image already exists in /srv. Use it? (y/n): " USE_EXISTING_RESPONSE
-    if [[ "$USE_EXISTING_RESPONSE" =~ ^[Yy]$ ]]; then
-        USE_EXISTING=true
-    else
-        wget --timeout=300 --tries=3 -q --show-progress -O "$IMAGE_PATH" "$IMAGE_URL"
-    fi
-else
-    wget --timeout=300 --tries=3 -q --show-progress -O "$IMAGE_PATH" "$IMAGE_URL"
-fi
+# Download Debian image file to temp folder
+TEMP_DIR=$(mktemp -d)
+wget --timeout=300 --tries=3 -q --show-progress -O "$TEMP_DIR/$IMAGE_NAME" "$IMAGE_URL"
 
-# Only download and verify checksum if not using an existing image
-if ! $USE_EXISTING; then
-    TEMP_DIR=$(mktemp -d)
-    CHECKSUMS_URL="${IMAGE_URL%/*}/SHA512SUMS"
-    wget -q -O "$TEMP_DIR/SHA512SUMS" "$CHECKSUMS_URL"
-    (cd "/srv" && grep "$IMAGE_NAME" "$TEMP_DIR/SHA512SUMS" | sha512sum -c --status)
-    rm -rf "$TEMP_DIR"
-fi
+# Verify checksum
+CHECKSUMS_URL="${IMAGE_URL%/*}/SHA512SUMS"
+wget -q -O "$TEMP_DIR/SHA512SUMS" "$CHECKSUMS_URL"
+(cd "$TEMP_DIR" && grep "$IMAGE_NAME" SHA512SUMS | sha512sum -c --status)
 
 # Prompt for username with validation
 while true; do
@@ -84,76 +70,32 @@ while true; do
     fi
 done
 
-# If using an existing image, check if the user already exists
-if $USE_EXISTING; then
-    if virt-customize -a "$IMAGE_PATH" --run-command "id -u $username" >/dev/null 2>&1; then
-        echo "[INFO] User '$username' already exists on the image."
-        USER_EXISTS=true
+# Prompt for password
+while true; do
+    read -s -p "Enter the password for the new user: " user_password
+    echo
+    read -s -p "Confirm the password: " user_password_confirm
+    echo
+    if [[ "$user_password" == "$user_password_confirm" ]]; then
+        break
     else
-        USER_EXISTS=false
+        echo "Passwords do not match."
     fi
-else
-    USER_EXISTS=false
-fi
-
-# Prompt for password only if user does not exist
-if ! $USER_EXISTS; then
-    while true; do
-        read -s -p "Enter the password for the new user: " user_password
-        echo
-        read -s -p "Confirm the password: " user_password_confirm
-        echo
-        if [[ "$user_password" == "$user_password_confirm" ]]; then
-            break
-        else
-            echo "Passwords do not match."
-        fi
-    done
-fi
-
-# Script functionality starts here
-
-# List .qcow2 files in /srv folder and select
-qcow2_files=($(ls /srv/*.qcow2 2>/dev/null))
-if [ ${#qcow2_files[@]} -eq 0 ]; then
-    echo "No .qcow2 files found in /srv."
-    exit 1
-elif [ ${#qcow2_files[@]} -gt 1 ]; then
-    select file_to_use in "${qcow2_files[@]}"; do
-        if [[ -n "$file_to_use" ]]; then
-            image_path="$file_to_use"
-            break
-        else
-            echo "Invalid selection."
-        fi
-    done
-else
-    image_path="${qcow2_files[0]}"
-fi
+done
 
 # Run virt-customize commands
-if ! $USER_EXISTS; then
-    virt-customize -a "$image_path" \
-        --install qemu-guest-agent,sudo,openssh-server \
-        --run-command "echo $HOSTNAME > /etc/hostname" \
-        --run-command "sed -i 's/\<localhost\>/$HOSTNAME/g' /etc/hosts" \
-        --run-command "useradd -m -s /bin/bash $username" \
-        --password "$username:password:$user_password" \
-        --run-command "usermod -aG sudo $username" \
-        --run-command "passwd -l root" \
-        --run-command "echo -n > /etc/machine-id"
-else
-    virt-customize -a "$image_path" \
-        --install qemu-guest-agent,sudo,openssh-server \
-        --run-command "echo $HOSTNAME > /etc/hostname" \
-        --run-command "old_hostname=\$(awk '/127.0.0.1/ {print \$2}' /etc/hosts)" \
-        --run-command "echo Old hostname: \$old_hostname" \
-        --run-command "sed -i \"s/^\(127\.0\.0\.1[[:space:]]\+\)\$old_hostname/\1$HOSTNAME/\" /etc/hosts" \
-        --run-command "echo -n > /etc/machine-id"
-fi
+virt-customize -a "$TEMP_DIR/$IMAGE_NAME" \
+    --install qemu-guest-agent,sudo,openssh-server \
+    --run-command "echo $HOSTNAME > /etc/hostname" \
+    --run-command "sed -i 's/\<localhost\>/$HOSTNAME/g' /etc/hosts" \
+    --run-command "useradd -m -s /bin/bash $username" \
+    --password "$username:password:$user_password" \
+    --run-command "usermod -aG sudo $username" \
+    --run-command "passwd -l root" \
+    --run-command "echo -n > /etc/machine-id"
 
 # Verify storage space
-image_size=$(stat -f --format="%s" "$IMAGE_PATH")
+image_size=$(stat -f --format="%s" "$TEMP_DIR/$IMAGE_NAME")
 storage_free=$(pvesm status -content images | awk -v storage="$STORAGE" '$1 == storage {print $4}')
 if [ "$image_size" -gt "$storage_free" ]; then
     echo "[ERROR] Insufficient storage space"
@@ -180,7 +122,7 @@ qm set "$VMID" --serial0 socket
 qm set "$VMID" --vga serial0
 
 # Import Debian disk image and set as primary boot disk
-qm importdisk "$VMID" "$IMAGE_PATH" "$STORAGE"
+qm importdisk "$VMID" "$TEMP_DIR/$IMAGE_NAME" "$STORAGE"
 qm set "$VMID" --scsi0 "$STORAGE:vm-$VMID-disk-0,discard=on,ssd=1,cache=none" --boot order=scsi0 --ostype l26
 
 echo "[INFO] VM creation completed successfully!"
